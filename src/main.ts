94782e7a0f74ab3163dd62784deb09c15ee4822e
oraclerevolution <mac.n.drix@gmail.com>
2023-07-08 10:24:18 +0000

    serve static files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import {NestExpressApplication} from '@nestjs/platform-express';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname,'..','images'))
+  app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('api');
   // configuration de Swagger
   const config = new DocumentBuilder()
